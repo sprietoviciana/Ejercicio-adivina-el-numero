7362e0832cc1d29fff9241e7f345f3d80ffc3ad4
Silvia Prieto <info@silviaprieto.com>
2024-10-25 10:04:11 +0200

    add js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 "use strict";
+
+const input = document.querySelector(".js-input");
+const button = document.querySelector(".js-button");
+const clue = document.querySelector(".js-clue");
+const numberTry = document.querySelector(".js-try");
+
+const randomNumber = getRandomNumber(100);
+let tries = 0;
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+
+function handleclick(event) {
+  event.preventDefault();
+  const inputNumber = input.value;
+
+  console.log(inputNumber);
+}
+
+button.addEventListener("click", handleclick);
